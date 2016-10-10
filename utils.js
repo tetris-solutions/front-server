7651949b84827c83ec88a9a7f5ddac444426fb4a
Christian Amaral <darthcas@gmail.com>
2016-10-10 17:17:05 -0300

    create utils module

---
+++ utils.js
@@ -0,0 +1,3 @@
+exports.saveResponseTokenAsCookie = require('./lib/functions/save-token-as-cookie').saveResponseTokenAsCookie
+exports.getApiFetchConfig = require('./lib/functions/get-api-fetch-config').getApiFetchConfig
+exports.pushResponseErrorToState = require('./lib/functions/push-response-error-to-state').pushResponseErrorToState
