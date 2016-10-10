1e994845166b7a35e90a6d7240eca6f2bc72bc9c
Christian Amaral <darthcas@gmail.com>
2016-10-10 17:37:55 -0300

    add pass-token-ahead to utils module

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
 exports.saveResponseTokenAsCookie = require('./lib/functions/save-token-as-cookie').saveResponseTokenAsCookie
 exports.getApiFetchConfig = require('./lib/functions/get-api-fetch-config').getApiFetchConfig
 exports.pushResponseErrorToState = require('./lib/functions/push-response-error-to-state').pushResponseErrorToState
+exports.passTokenAhead = require('./lib/functions/pass-token-ahead').passTokenAhead
